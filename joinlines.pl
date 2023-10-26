647f6462f7c8ad0b4d056b7a2445289cbba3d9c3
bbryson <Beth-docs_Bryson@sil.org>
2023-10-26 18:27:47 -0500

    Adjust comments slightly

--- joinlines.pl
+++ joinlines.pl
@@ -3,6 +3,7 @@
 # Usage: joinlines.pl < infile > outfile
 
 # Assumes an SFM file, with backslash codes marking the fields.
+# Assumes Unix line endings.
 # Finds any fields that span multiple lines and puts them onto a single line.
 
 # First strips trailing spaces from each line.
@@ -13,10 +14,10 @@
 # - make sure there are no blank lines in the middle of records.
 # - make sure the file ends with a blank line.
 
-# Created:	  8 Mar 2004	bb
+# Modified:	07 Jun 2023		bb	Improve line end handling, remove trailing blanks,
+#													   add more comments
 # Modified:	13 Feb 2020	bb	Add comments
-# Modified:	07 Jun 2023		bb	Improve line end handling, remove trailing blanks
-#													more comments
+# Created:	  8 Mar 2004	bb
 
 # Count how many lines were joined, for verification.
 $i = 0;
